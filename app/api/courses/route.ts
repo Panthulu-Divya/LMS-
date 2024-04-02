b385f6588617fce983a26f09d6cfbd8bf81f8e1b
Ritik Singh <ritiks236@gmail.com>
2024-04-02 21:04:19 +0530

    course api created

---
+++ app/api/courses/route.ts
@@ -0,0 +1,28 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function POST(req:Request) {
+    try{
+        const { userId } = auth();
+        const { title } = await req.json();
+        
+        if(!userId){
+            return new NextResponse("unauthorized", {status: 401})
+        }
+
+        const course = await db.course.create({
+            data:{
+                userId,
+                title,
+            }
+        });
+
+        return NextResponse.json(course); 
+
+    } catch (err) {
+        console.log("Erorr in courses", err);
+        return new NextResponse("Internal Error", {status: 500});
+    }
+}
