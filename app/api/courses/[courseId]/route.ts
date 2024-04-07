9862b4470c966412b2f03570229c96453965f468
Ritik Singh <ritiks236@gmail.com>
2024-04-07 23:25:31 +0530

    course added

---
+++ app/api/courses/[courseId]/route.ts
@@ -0,0 +1,33 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { error } from "console";
+import { NextResponse } from "next/server";
+
+export async function PATCH(req: Request, {params} : {params: {courseId: string}}) {
+    try{
+        const {userId} = auth();
+        const { courseId } = params;
+        const values = await req.json();
+
+        if(!userId){
+            return new NextResponse("Unauthorized", {status: 401});
+        }
+
+        const course = await db.course.update({
+            where:{
+                id: courseId,
+                userId
+            },
+            data:{
+                ...values,
+            }
+        })
+
+        return NextResponse.json(course);
+
+    } catch (err) {
+        console.log("[COURSE_ID]", error);
+        return new NextResponse("Internal Erro", {status: 500})
+    }
+    
+}
